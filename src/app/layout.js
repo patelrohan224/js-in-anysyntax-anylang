9a4bd4aa7b3ed9957bb69cd032914975101dc858
Patel Rohan <rohan_fw11_289@masai.school>
2023-12-26 14:32:12 +0000

    added monitoring items

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/Provider";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <Provider>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </Provider>
       </body>
     </html>
   );
